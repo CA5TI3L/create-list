11d4fbbdaa2c6db448bfa414e9d8e1c1fa4a58f0
Kevin <kevinterblanche16@gmail.com>
2020-10-04 19:27:51 +0200

    Send data to list container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'create-list';
+  item;
+
+  sendListData(item) {
+    this.item = item;
+  }
 }
--- src/app/side-bar/side-bar.component.ts
+++ src/app/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy  } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, EventEmitter, Output  } from '@angular/core';
 
 @Component({
   selector: 'app-side-bar',
@@ -9,6 +9,7 @@ import { Component, OnInit, ChangeDetectionStrategy  } from '@angular/core';
 export class SideBarComponent implements OnInit {
 
   userLists = new Array();
+  @Output() passListData : EventEmitter<any> = new EventEmitter();
 
 
 
@@ -23,6 +24,10 @@ export class SideBarComponent implements OnInit {
       listItems: []
     }
     this.userLists.push(temp)
+  }
+
+  changeCurrentList(item) {
+    this.passListData.emit(item);
   }
 
 }
